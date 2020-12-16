e55c4cc570b784ca343414c93d2aaef1087db43b
ObiekweAgbu <49278842+ObiekweAgbu@users.noreply.github.com>
2020-12-16 21:16:36 +0000

    obiekwe

--- SmartCare CW/SmartCareApp/src/java/pages/LoginController.java
+++ SmartCare CW/SmartCareApp/src/java/pages/LoginController.java
@@ -1,3 +1,5 @@
+package pages;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,6 +7,7 @@
  */
 
 
+import model.LoginData;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
